e262010cef0f8ae23228ce682a0cddd444ed89ea
Bestedeki <132979090+Bestedeki@users.noreply.github.com>
2023-07-04 13:41:26 -0600

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -6,7 +6,7 @@ sonarqubeScanner({
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsAcadalearning',
-	    'sonar.login': '2ec3d7d9ecac86ca3d26da44a4be44a205bdbed8',
+	    'sonar.login': 'cf46ccd729fbcff685bb4c58e732dde14c7cea78',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
